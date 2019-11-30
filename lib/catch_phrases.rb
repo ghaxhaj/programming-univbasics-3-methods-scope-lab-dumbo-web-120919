6da9b51e4b9836a13f97b576cf24053ea28493d2
wise-integer-6834 <ghaxhaj@fordham.edu>
2019-11-30 23:07:38 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,12 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
   phrase = "It's-a me, Mario!"
+  puts phrase
 end
 
 def toadstool
   puts status
 end
+
+mario
+toadstool
